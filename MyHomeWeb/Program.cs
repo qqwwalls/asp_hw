251caad0d1cc8be2bc60e38b17c1a64035d309c2
Olena <olena@example.com>
2026-06-27 19:35:55 +0300

    Initial commit: ASP.NET Core Web API homework tasks

---
+++ MyHomeWeb/Program.cs
@@ -0,0 +1,7 @@
+var app = WebApplication.Create(args);
+
+app.MapGet("/", () => "Welcome to MyHomeWeb!");
+app.MapGet("/hello/{name}", (string name) => $"Hello, {name}!");
+app.MapGet("/api/status", () => new { message = "MyHomeWeb is running", time = DateTime.Now });
+
+app.Run();
